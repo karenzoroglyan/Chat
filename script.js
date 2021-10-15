299974f75adc4c028f847bf48de76866fc1f12bf
Karen <karen.zoroglyan@mail.ru>
2021-10-15 17:59:51 +0400

    feat: new css template

--- script.js
+++
@@ -1,8 +0,0 @@
-message__input.onclick = function () {
-  let div = document.querySelector(".contacts");
-  div.innerHTML = " ";
-  let element = document.createElement("div");
-  element.innerHTML = "Karen Zoroglyan";
-  element.className = "contacts__item1";
-  div.append(element);
-};
